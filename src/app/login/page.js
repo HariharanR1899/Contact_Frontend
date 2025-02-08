bdbb17b67679159be3d2cbc9555fa8c17a493581
Hariharan R <hariharan.r1899@gmail.com>
2025-02-08 19:31:48 +0000

    testing response.data

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -13,17 +13,15 @@ export default function Login() {
     e.preventDefault();
     try {
       const response = await axios.post(`${BASE_URL}/login`, formData);
+      console.log(response.data);
       localStorage.setItem("token", response.data.token);
       localStorage.setItem("user", JSON.stringify(response.data.user));
-      window.location.href = "/"; 
+    //   window.location.href = "/"; 
     } catch (error) {
       console.error("Login failed:", error);
       alert("Invalid credentials. Please try again.");
     }
-    finally{
-        window.location.href = "/"; 
-        console.log("testtt");
-    }
+  
   };
 
   return (
