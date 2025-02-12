daf481da491cc8359aefc35a972e77bd8635022d
Hariharan R <hariharan.r1899@gmail.com>
2025-02-12 01:57:12 +0000

    .

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -1,6 +1,6 @@
 
 "use client";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 import styles from "../auth.module.css"; // ✅ Import CSS
